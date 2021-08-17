0b245f32cefdf44a4438c813804a8c64867c6b27
SerkaNem <72761335+SerkaNem@users.noreply.github.com>
2021-08-17 16:13:41 +0200

    Add files via upload

---
+++ Sources/JavaScript/FooterScrypt.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName('footer')[0].innerHTML = `
+	<a href="Accueil.html"><img src="../../image/Logo/Logo.png" alt="DTRE" /></a>
+	<p>Nous contacter :</p>
+	<p>Adresse mail : <br />
+	<a href="mailto:dtre@et.esiea.fr">dtre@et.esiea.fr</a></p>
+	<a href="#"><img src="../../image/Réseaux Sociaux/Logo YouTube.jpg" alt="Chaîne YouTube" /></a>
+	<a href="https://www.instagram.com/dtre_esiea/"><img src="../../image/Réseaux Sociaux/Logo Instagram.png" alt="Instagram" /></a>
+	<a href="https://discord.gg/aQpkhjkGeV"><img src="../../image/Réseaux Sociaux/Logo Discord.png" alt="Discord"></a>
+	<a href="https://twitter.com/assodtre"><img src="../../image/Réseaux Sociaux/Logo Twitter.png" alt="Twitter" /></a>
+	<a href="Accueil.html"><img src="../../image/Logo/Logo.png" alt="DTRE" /></a>
+`;
---
+++ Sources/JavaScript/headerScrypt.js
@@ -0,0 +1,20 @@
+document.getElementById('header').innerHTML = 
+`
+	<div id="titrePrincipal">
+		<div id="logo">
+			<img src="../../image/Logo/Logo.png" alt="Logo DTRE" id="logoDTRE" />
+			<h1>DTRE</h1>
+		</div>
+		<h4>Département Technique & Robotique ESIEA</h4>
+	</div>
+				
+	<nav>
+		<ul>
+			<li><a href="Accueil.html">Accueil</a></li>
+			<li><a href="Rejoindre.html">Nous rejoindre</a></li>
+			<li><a href="Equipe.html">L'équipe</a></li>
+			<li><a href="Projets.html">Projets</a></li>
+			<li><a href="Partenaires.html">Partenaires</a></li>
+		</ul>
+	</nav>
+`;
